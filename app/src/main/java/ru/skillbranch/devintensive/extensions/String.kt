f6c45e98019be26b744d059d2bb3c6ac02bfd550
Alexander Baran <bunin@severalnines.com>
2019-07-04 00:15:23 +0700

    *String fixes

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,14 +1,14 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(n: Int = 16): String? {
-    if (this.trim().length <= n) {
-        return this.trim()
+    if (this.trimEnd().length <= n) {
+        return this.trimEnd()
     }
     if (n < 4) {
         return this.substring(0, 2)
     }
 //    return this.trim().substring(0..kotlin.math.min(this.trim().length - 4, n - 4)).trim() + "..."
-    return this.trim().substring(0 until n).trim() + "..."
+    return this.trimEnd().substring(0 until n).trimEnd() + "..."
 }
 
 fun String.stripHtml(): String {
